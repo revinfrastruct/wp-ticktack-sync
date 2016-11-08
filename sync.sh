7c3b5172ba8c905a5f99b326ef1986945292fe23
alfred <alfred@nurd.nu>
2016-11-08 02:36:29 +0000

    delete ticks not in wordpress

--- sync.sh
+++ sync.sh
@@ -5,6 +5,8 @@ if [ "$EVENTSLUG" = "" ]; then
   exit 1
 fi
 
+DELTICKS="$($TICKTACK list | jq -r '. | keys | .[]')"
+
 TICKS="$($WPCLI post list --post_type=livetick --post_status=publish --posts_per_page=10000 --format=json | jq '.[].ID')"
 for ID in $TICKS; do
   TICKDATA="$($WPCLI post get $ID --format=json)"
@@ -15,7 +17,12 @@ for ID in $TICKS; do
     TICKCONTENT="$(echo "$TICKDATA" | jq -r .post_content)"
     TICKEPOCH="$(date -d "$TICKDATE" +'%s')"
 
-    echo "$TICKTACK set --time $TICKEPOCH $ID"
     echo "$TICKCONTENT" | $TICKTACK set --time $TICKEPOCH $ID
+
+    DELTICKS="$(echo "$DELTICKS" | grep -v $ID)"
   fi
+done
+
+for ID in $DELTICKS; do
+  $TICKTACK del $ID
 done
